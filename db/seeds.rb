0d65ba741acde071d6e9e9134b5d309fa3b3e952
krdiamond <github email address>
2018-02-06 11:00:24 -0500

    solved lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Artist.create(name: "Red House Painters", bio: "Mark Kozolik's band, slow sad shit.")
+Artist.create(name: "The Replacements", bio: "Late 70s poppy punk")
+Artist.create(name: "The Ramones", bio: "original punk, every song sounds the same")
+Song.create(name: "New Jersey", artist_id: 1, genre_id:2)
+Song.create(name: "Can't Hardly Wait", artist_id:2, genre_id:1)
+Song.create(name: "Do You Want to Dance?", artist_id: 3, genre_id: 1)
+Genre.create(name: "Punk")
+Genre.create(name: "Slow Sad Shit")
